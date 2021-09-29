4169942f615ab6fc2060315e4e6868ecbf3f41de
Aballey Wilson <willywallet001@gmail.com>
2021-09-29 22:59:51 +0000

    first commit

---
+++ Maximize it.py
@@ -0,0 +1,17 @@
+from itertools import product
+from math import pow
+it = list(map(int,input().split()))
+a , b = it[0],it[1]
+listings=[]
+
+for i in range(a):
+    listings.append(list(map(int,input().split()))[1:])
+
+M = list(product(*listings))
+final_answer =[]
+for i in M:
+    sum = 0
+    for j in i:
+        sum = sum + pow(j,2)
+    final_answer.append(sum % b)
+print(max(final_answer))
