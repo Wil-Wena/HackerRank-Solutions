7a7a0fc275c691452414f30114e6eb84e40eee55
Aballey Wilson <willywallet001@gmail.com>
2021-10-08 14:20:52 +0000

    Update Merge the tools.py

--- Merge the tools.py
+++ Merge the tools.py
@@ -11,11 +11,11 @@ string, k = input(), int(input())
 def merge_the_tools(string, k):
     while string:
         s = string[0:k] #String index 
-        seen = ''
+        m = ''
         for c in s:
-            if c not in seen:
-                seen += c  #Appends each letter
-        print(seen)
+            if c not in m:
+                m = m + c  #Appends each letter
+        print(m)
         string = string[k:] #Sets string to the current index not printe out.
 
 #Function call
