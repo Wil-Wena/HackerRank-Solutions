7ac514869a5ac8a0172f6e0ee83e812541e18030
Wilson Aballey <willywallet001@gmail.com>
2021-10-12 15:26:36 +0000

    Array

---
+++ Data Structures/Arrays.py
@@ -0,0 +1,21 @@
+#By: Aballey Wilson
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the 'reverseArray' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY a as parameter.
+
+ #Slicing method
+def reverseArray(a):
+    m = a[::-1] #Starts printing from step(-1) which is the last value
+    return m
+
+m = input()
+print(reverseArray(m))
+
